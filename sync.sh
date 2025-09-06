bf4389077e0a29fdd1aa0934eae3d6e5b2a1d8d2
Daniil <maleshkodaniil@gmail.com>
2025-09-07 00:03:04 +0300

    backup 2025-09-07

--- sync.sh
+++ sync.sh
@@ -2,10 +2,10 @@
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-[ -e "~/.zshrc" ] && cp ~/.zshrc $CURRENT_DIR
-[ -e "~/.zshenv" ] && cp ~/.zshenv $CURRENT_DIR
-[ -e "~/.zprofile" ] && cp ~/.zprofile $CURRENT_DIR
-[ -e "~/.xinitrc" ] && cp ~/.xinitrc $CURRENT_DIR
+[ -f "~/.zshrc" ] && cp ~/.zshrc $CURRENT_DIR
+[ -f "~/.zshenv" ] && cp ~/.zshenv $CURRENT_DIR
+[ -f "~/.zprofile" ] && cp ~/.zprofile $CURRENT_DIR
+[ -f "~/.xinitrc" ] && cp ~/.xinitrc $CURRENT_DIR
 
 git add .
 git commit -qm "backup $(date '+%Y-%m-%d')"
