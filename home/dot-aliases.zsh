e585d799d5379fc56b47c55684f4ff34c42d65b2
strtab <strtab>
2025-12-26 02:18:15 +0300

    zsh update

--- home/dot-aliases.zsh
+++ home/dot-aliases.zsh
@@ -1,14 +1,11 @@
+##### Common
 alias j='jobs'
 alias ip='ip -c=auto'
-alias gc='git clone --depth 1'
 alias du='du -hs'
-alias procs="ps -eo uid,exe | grep $UID | awk '{print $2}' | xargs -n1 basename | sort | uniq -c | fzf --no-preview"
 alias ku="kubectl"
+alias e='echo "$?"'
 
-alias s="yay -Ss"
-alias i="yay -S"
-
-# bat hightlights
+##### Bat hightlights
 alias cat='bat --no-pager'
 alias ports='ss -tulan | bat -l log'
 alias cpuinfo='bat -l cpuinfo /proc/cpuinfo'
@@ -16,16 +13,29 @@ alias df='df -hx tmpfs -x efivarfs -x swap 2>&1 | bat -l lua'
 alias free='free -h 2>&1 | bat -l cpuinfo'
 alias lsblk='lsblk -i -o NAME,FSTYPE,SIZE,FSUSE%,MOUNTPOINTS,UUID 2>&1 | bat -l conf'
 alias fzf='fzf --preview "bat --color=always --style=numbers --line-range=:500 {}"'
-alias -g -- -h='-h 2>&1 | bat --language=help --style=plain'
-alias -g -- --help='--help 2>&1 | bat --language=help --style=plain'
+alias -g -- --help='--help 2>&1 | bat --language=help --style=plain' 
 
-# neovim
-alias vi='nvim'
-alias svi='sudo nvim'
+# unalias l
+# l() {
+#   ls -l -p -h --literal --sort=extension --time-style=long-iso -G "$@" |\
+#     awk 'NR==1 {print; next} {print $1, $3, $7, $8, $9}' |\
+#     bat --no-pager --style=plain -l fstab
+# }
 
-# eza
+# unalias ll
+# ll() {
+#   ls -l -p -A -h --literal --sort=extension --time-style=long-iso -G "$@" |\
+#     awk 'NR==1 {print; next} {print $1, $3, $7, $8, $9}' |\
+#     bat --no-pager --style=plain -l fstab
+# }
+
+##### Eza
 alias tree='eza --tree -L 2'
-alias ls='eza' 
 alias ll='eza -la --no-time'
 alias la='eza -a --no-time'
 alias l='eza -l --no-time'
+alias ls='eza' 
+
+##### neovim
+alias vi='nvim'
+alias svi='sudo -e' # this use $EDITOR variable
